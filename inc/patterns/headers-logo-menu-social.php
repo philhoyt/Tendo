5d9d50ae4c958cd77688adf4cbf86881a814d10b
Phil Hoyt <hoytph@gmail.com>
2022-01-05 21:12:56 -0500

    Cleanup header patterns

--- inc/patterns/headers-logo-menu-social.php
+++ inc/patterns/headers-logo-menu-social.php
@@ -5,7 +5,7 @@ return array(
 	'content'    => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"1em","right":"1em","bottom":"1em","left":"1em"}}},"layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between"}} -->
 	<div class="wp-block-group" style="padding-top:1em;padding-right:1em;padding-bottom:1em;padding-left:1em"><!-- wp:site-title /-->
 	
-	<!-- wp:navigation {"ref":11,"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"typography":{"fontStyle":"italic","fontWeight":"700","textTransform":"lowercase"}},"fontFamily":"arial-helvetica"} /-->
+	<!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"textTransform":"lowercase"}},"fontFamily":"arial-helvetica"} /-->
 	
 	<!-- wp:social-links {"iconColor":"black","iconColorValue":"#000000","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"left"}} -->
 	<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"twitter"} /-->
--- inc/patterns/headers-utility.php
+++ inc/patterns/headers-utility.php
@@ -3,10 +3,10 @@ return array(
 	'title'      => __( 'Logo, menu, with utility menu', 'tendo' ),
 	'categories' => array( 'tendo-headers' ),
 	'content'    => '<!-- wp:group {"backgroundColor":"black","textColor":"white"} -->
-	<div class="wp-block-group has-white-color has-black-background-color has-text-color has-background"><!-- wp:navigation {"ref":36,"overlayMenu":"never","layout":{"type":"flex","justifyContent":"right"},"style":{"typography":{"fontSize":"0.8em"}}} /--></div>
+	<div class="wp-block-group has-white-color has-black-background-color has-text-color has-background"><!-- wp:navigation {"textColor":"white","overlayMenu":"never","layout":{"type":"flex","justifyContent":"right"},"style":{"typography":{"fontSize":"0.8rem"}},"fontFamily":"courier-new"} /--></div>
 	<!-- /wp:group --><!-- wp:group {"style":{"spacing":{"padding":{"top":"1em","right":"1em","bottom":"1em","left":"1em"}}},"layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between"}} -->
 	<div class="wp-block-group" style="padding-top:1em;padding-right:1em;padding-bottom:1em;padding-left:1em"><!-- wp:site-title /-->
 	
-	<!-- wp:navigation {"ref":11,"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"typography":{"fontStyle":"italic","fontWeight":"700","textTransform":"lowercase"}},"fontFamily":"arial-helvetica"} /--></div>
+	<!-- wp:navigation {"layout":{"type":"flex","justifyContent":"right"},"style":{"typography":{"textTransform":"lowercase"}},"fontFamily":"arial-helvetica"} /--></div>
 	<!-- /wp:group -->',
 );
